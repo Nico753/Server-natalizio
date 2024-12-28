ab9f66cbd390ea6ad72c95466a731a2b159136cf
Nico753 <n.dominici.3067@vallauri.edu>
2024-12-28 15:04:40 +0100

    Update data.js

--- functions/data.js
+++ functions/data.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
-// Percorso al file JSON che si trova nella cartella 'public'
+// Percorso al file JSON
 const jsonFilePath = path.join(__dirname, '../public/data.json');
 
-exports.handler = async function(event, context) {
+exports.handler = async function(event) {
   try {
-    const data = fs.readFileSync(jsonFilePath, 'utf8');
+    const data = await fs.readFile(jsonFilePath, 'utf8');
     const currentData = JSON.parse(data);
 
     return {
@@ -16,7 +16,7 @@ exports.handler = async function(event, context) {
   } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Errore nella lettura del file' })
+      body: JSON.stringify({ error: 'Errore nella lettura del file', details: err.message })
     };
   }
 };
