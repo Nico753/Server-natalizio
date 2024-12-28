92ddb297094e061f5fbfa883b58052ebb57b7320
Nico753 <n.dominici.3067@vallauri.edu>
2024-12-28 16:00:17 +0100

    Update add-user.js

--- functions/add-user.js
+++ functions/add-user.js
@@ -1,30 +1,30 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-// Percorso al file JSON
-const jsonFilePath = path.join(__dirname, '../public/data.json');
-
 exports.handler = async function(event, context) {
+  const tempJsonFilePath = '/tmp/data.json'; // Percorso nel file system temporaneo
+
   if (event.httpMethod === 'POST') {
     // Estrai il nuovo utente dal body della richiesta
-    const newUser = JSON.parse(event.body);  // Supponiamo che tu invii l'utente come JSON
+    const newUser = JSON.parse(event.body);
 
     try {
-      // Leggi il file esistente
-      const data = await fs.readFile(jsonFilePath, 'utf8');
+      // Prima copiamo il file "data.json" dalla cartella "public" a "/tmp"
+      const publicJsonFilePath = path.join(__dirname, '../public/data.json');
+      const data = await fs.readFile(publicJsonFilePath, 'utf8');
       const currentData = JSON.parse(data);
 
       // Aggiungi il nuovo utente
       currentData.Users.push(newUser);
 
-      // Scrivi i dati aggiornati nel file JSON
-      await fs.writeFile(jsonFilePath, JSON.stringify(currentData, null, 2));
+      // Scrivi i dati aggiornati nel file temporaneo
+      await fs.writeFile(tempJsonFilePath, JSON.stringify(currentData, null, 2));
 
       return {
         statusCode: 200,
         body: JSON.stringify({ message: 'Nuovo utente aggiunto', newUser }),
         headers: {
-          'Access-Control-Allow-Origin': '*',  // Permetti tutte le origini
+          'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
           'Access-Control-Allow-Headers': 'Content-Type',
         }
@@ -34,7 +34,7 @@ exports.handler = async function(event, context) {
         statusCode: 500,
         body: JSON.stringify({ error: 'Errore nella scrittura del file', details: err.message }),
         headers: {
-          'Access-Control-Allow-Origin': '*',  // Permetti tutte le origini anche in caso di errore
+          'Access-Control-Allow-Origin': '*',
         }
       };
     }
@@ -44,7 +44,7 @@ exports.handler = async function(event, context) {
     statusCode: 405,
     body: JSON.stringify({ error: 'Metodo non supportato' }),
     headers: {
-      'Access-Control-Allow-Origin': '*',  // Permetti tutte le origini anche in caso di errore
+      'Access-Control-Allow-Origin': '*',
     }
   };
 };
