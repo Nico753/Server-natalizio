181ecfb388d9ae66449d9aaad499f876652b0be3
Nico753 <n.dominici.3067@vallauri.edu>
2024-12-28 15:58:06 +0100

    Update add-user.js

--- functions/add-user.js
+++ functions/add-user.js
@@ -22,23 +22,29 @@ exports.handler = async function(event, context) {
 
       return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'Nuovo utente aggiunto', newUser })
+        body: JSON.stringify({ message: 'Nuovo utente aggiunto', newUser }),
         headers: {
-        'Access-Control-Allow-Origin': '*',  // Permetti tutte le origini
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
-      }
+          'Access-Control-Allow-Origin': '*',  // Permetti tutte le origini
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type',
+        }
       };
     } catch (err) {
       return {
         statusCode: 500,
-        body: JSON.stringify({ error: 'Errore nella scrittura del file', details: err.message })
+        body: JSON.stringify({ error: 'Errore nella scrittura del file', details: err.message }),
+        headers: {
+          'Access-Control-Allow-Origin': '*',  // Permetti tutte le origini anche in caso di errore
+        }
       };
     }
   }
 
   return {
     statusCode: 405,
-    body: JSON.stringify({ error: 'Metodo non supportato' })
+    body: JSON.stringify({ error: 'Metodo non supportato' }),
+    headers: {
+      'Access-Control-Allow-Origin': '*',  // Permetti tutte le origini anche in caso di errore
+    }
   };
 };
